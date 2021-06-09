efa8b70de059ab1465198e628e816b999c8e2a50
Aniket-123-bit <anisunny782@gmail.com>
2021-06-09 12:24:34 +0530

    commit uc2 Push

---
+++ src/com/snake/uc2.java
@@ -0,0 +1,14 @@
+package com.snake;
+
+public class uc2 {
+	public static void main(String args[]) {
+		System.out.println("Welcome to SnakeAndLadder Game");
+		//intial position 
+		int playerPosition = 0;
+		//Computation
+		int rolldice = (int) (Math.floor(Math.random()*(6)+1));
+		System.out.println("OutCome: "+rolldice);
+		
+	}
+
+}
